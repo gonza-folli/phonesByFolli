2fdbe6d0fdd66c78374fc020d59d65b088f09750
Gonzalo <gonza_folli@hotmail.com>
2021-10-18 12:06:39 -0300

    itemcount

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './ItemCount.css'
+export const ItemCount = ( props ) => {
+
+    const [itemCount, setItemcount] = useState(Number(props.initial))
+    const onAdd = () => {
+        if (itemCount < props.stock)
+        setItemcount(itemCount + 1)
+    }
+    const onRemove = () => {
+        if (itemCount > 1) 
+        setItemcount(itemCount - 1)
+    }
+
+    return <div className="productQuantity">
+        <button onClick={onRemove}>-</button>
+        <h1>{itemCount}</h1>
+        <button onClick={onAdd}>+</button>
+    </div>
+}
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,10 @@
+import { ItemCount } from '../ItemCount/ItemCount'
+// import { useState } from 'react'
 import './ItemListContainer.css'
 
+
 export const ItemListContainer = (props) => {
+
     return <section className="ItemListContainer">
         <h1 className="sectionTitle">{props.greeting}</h1>
 
@@ -10,6 +14,7 @@ export const ItemListContainer = (props) => {
                 <img alt="phone1" className="productImg" src="https://www.apple.com/v/iphone/home/ba/images/overview/compare/compare_iphone_13_pro__etecge8yu8q6_large.jpg" />
                 <h2 className="productTitle">iPhone 13 PRO</h2>
                 <h4 className="productDesc">El iPhone en su máxima expresión</h4>
+                <ItemCount stock="5" initial="1" />
                 <button className="addBtn">Comprar</button>
             </div>
 
@@ -17,6 +22,7 @@ export const ItemListContainer = (props) => {
                 <img alt="phone1" className="productImg" src="https://www.apple.com/v/iphone/home/ba/images/overview/compare/compare_iphone_13__ejsyn7suyw02_large.jpg" />
                 <h2 className="productTitle">iPhone 13</h2>
                 <h4 className="productDesc">Una potencia extraordinaria.</h4>
+                <ItemCount stock="7" initial="1" />
                 <button className="addBtn">Comprar</button>
             </div>
 
@@ -24,6 +30,7 @@ export const ItemListContainer = (props) => {
                 <img alt="phone1" className="productImg" src="https://www.apple.com/v/iphone/home/ba/images/overview/compare/compare_iphone_12__edurff15kyoi_large.jpg" />
                 <h2 className="productTitle">iPhone 12</h2>
                 <h4 className="productDesc">Increíble como siempre.</h4>
+                <ItemCount stock="3" initial="1" />
                 <button className="addBtn">Comprar</button>
             </div>
 
